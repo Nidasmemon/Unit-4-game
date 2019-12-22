88a96b7a7d90550557ae6a8eb8f16a7880fb432f
Nida Memon <nidasmemon@gmail.com>
2019-12-22 15:01:26 -0800

    completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Unit-4-game
+
+This is a guessing game where the computer generates a random number for you to match using jewels that also have random numbers assigned to them. Every time you click a jewel, your number increases by the amount set to that specific jewel. If you match the random number exactly, you win. However, if you go over, you lose. The game is then restarted for you to play again.  
+
+Hope you enjoy playing Crystals Collector! 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,63 @@
-// Creating functions to generate random numbers.
-function randGen() {
-    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    return randomNumber;
+ // Declaring variables to hold the score, and the number of wins and losses.
+ var scoreNum = 0;
+ var wins = 0;
+ var losses = 0;
+
+ 
+ // Declaring variables to hold the random numbers.
+ var randomNumber = randGen();
+ var crystalNum1 = randCrystal1();
+ var crystalNum2 = randCrystal2();
+ var crystalNum3 = randCrystal3();
+ var crystalNum4 = randCrystal4();
+
+ 
+ // Creating functions to generate random numbers.
+ function randGen() {
+     randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+     return randomNumber;
+ }
+
+ function randCrystal1() {
+     crystalNum1 = Math.floor(Math.random() * 12) + 1;
+     return crystalNum1;
+ }
+
+ function randCrystal2() {
+     crystalNum2 = Math.floor(Math.random() * 12) + 1;
+     return crystalNum2;
+ }
+
+ function randCrystal3() {
+     crystalNum3 = Math.floor(Math.random() * 12) + 1;
+     return crystalNum3;
+ }
+
+ function randCrystal4() {
+     crystalNum4 = Math.floor(Math.random() * 12) + 1;
+     return crystalNum4;
+ }
+
+
+ // Creating a function to restart the game.
+ function restart() {
+    randomNumber = randGen();
+    crystalNum1 = randCrystal1();
+    crystalNum2 = randCrystal2();
+    crystalNum3 = randCrystal3();
+    crystalNum4 = randCrystal4();
+    scoreNum = 0;
+    $("#score").text(scoreNum);
+    $("#randomNum").text(randomNumber);
+
+
+    console.log("Restart function has been called with these values.")
+    console.log("Random Number: " + randomNumber);
+    console.log("Crystal Num1: " + crystalNum1);
+    console.log("Crystal Num2: " + crystalNum2);
+    console.log("Crystal Num3: " + crystalNum3);
+    console.log("Crystal Num4: " + crystalNum4);
 }
-
-function randCrystal1() {
-    crystalNum1 = Math.floor(Math.random() * 12) + 1;
-    return crystalNum1;
-}
-
-function randCrystal2() {
-    crystalNum2 = Math.floor(Math.random() * 12) + 1;
-    return crystalNum2;
-}
-
-function randCrystal3() {
-    crystalNum3 = Math.floor(Math.random() * 12) + 1;
-    return crystalNum3;
-}
-
-function randCrystal4() {
-    crystalNum4 = Math.floor(Math.random() * 12) + 1;
-    return crystalNum4;
-}
-
-
-// Declaring variables to hold the random numbers.
-var randomNumber = randGen();
-var crystalNum1 = randCrystal1();
-var crystalNum2 = randCrystal2();
-var crystalNum3 = randCrystal3();
-var crystalNum4 = randCrystal4();
-
-
-// Declaring variables to hold the score, and the number of wins and losses.
-var scoreNum = 0;
-var wins = 0;
-var losses = 0;
 
 
 $(document).ready(function() {
@@ -46,53 +67,80 @@ $(document).ready(function() {
     $("#randomNum").text(randomNumber);
 
     $("img#red").on("click", function() {
-        $(this).val(crystalNum1);
-        $("#score").text(crystalNum1);
+        scoreNum = scoreNum + crystalNum1;
+        $("#score").text(scoreNum);
+
+        if (scoreNum === randomNumber) {
+            wins++;
+            $("#win").text(wins);
+            restart();
+        }
+     
+        else if (scoreNum > randomNumber) {
+            losses++;
+            $("#lose").text(losses);
+            restart();
+        }
     });
 
     $("img#blue").on("click", function() {
-        $(this).val(crystalNum2);
-        $("#score").text(crystalNum2);
+        scoreNum = scoreNum + crystalNum2;
+        $("#score").text(scoreNum);
+
+        if (scoreNum === randomNumber) {
+            wins++;
+            $("#win").text(wins);
+            restart();
+        }
+     
+        else if (scoreNum > randomNumber) {
+            losses++;
+            $("#lose").text(losses);
+            restart();
+        }
     });
 
     $("img#orange").on("click", function() {
-        $(this).val(crystalNum3);
-        $("#score").text(crystalNum3);
+        scoreNum = scoreNum + crystalNum3;
+        $("#score").text(scoreNum);
+
+        if (scoreNum === randomNumber) {
+            wins++;
+            $("#win").text(wins);
+            restart();
+        }
+     
+        else if (scoreNum > randomNumber) {
+            losses++;
+            $("#lose").text(losses);
+            restart();
+        }
     });
 
     $("img#green").on("click", function() {
-        $(this).val(crystalNum4);
-        $("#score").text(crystalNum4);
+        scoreNum = scoreNum + crystalNum4;
+        $("#score").text(scoreNum);;
+
+        if (scoreNum === randomNumber) {
+            wins++;
+            $("#win").text(wins);
+            restart();
+        }
+     
+        else if (scoreNum > randomNumber) {
+            losses++;
+            $("#lose").text(losses);
+            restart();
+        }
     });
 
 
-    // Creating function to restart the game.
-    function restart() {
-        randGen();
-        randCrystal1();
-        randCrystal2();
-        randCrystal3();
-        randCrystal4();
-    }
-
+    console.log("Random Number: " + randomNumber);
+    console.log("Crystal Num1: " + crystalNum1);
+    console.log("Crystal Num2: " + crystalNum2);
+    console.log("Crystal Num3: " + crystalNum3);
+    console.log("Crystal Num4: " + crystalNum4);
     
-    // Determines the outcome of the game.
-    if (scoreNum === randomNumber) {
-        wins++;
-        restart();
-    }
- 
-    if (scoreNum > randomNumber) {
-        losses++;
-        restart();
-    }
-
-
-    console.log(randomNumber);
-    console.log(crystalNum1);
-    console.log(crystalNum2);
-    console.log(crystalNum3);
-    console.log(crystalNum4);
 
 });
 
